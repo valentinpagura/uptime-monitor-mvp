99394e2cc3f03e55a40a31f4bc7b44a7cafcbd95
valentinpagura <paguravalentin@mail.com>
2026-05-16 20:34:20 +0000

    Day 3: Add cors and dotenv configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,22 @@
-const express = require("express");  //importamos la libreria de express y la guardamos en la variable "express"
-const app = express();                //creamos una instancia de express y la guardamos en la variable "app"
+//IMPORTACIONES
+const dotenv = require('dotenv');  //dotenv → saca valores del archivo .env y los pone en process.env
+dotenv.config();
+const cors = require('cors');
 
+
+//MOLDE
+const express = require("express");  //importamos la libreria de express y la guardamos en la variable "express"
+const app = express();  //creamos una instancia de express y la guardamos en la variable "app"
+
+//CONFIGURACION DEL SERVIDOR
+app.use(cors({ origin: process.env.CLIENT_URL }))  //configuramos cors para que solo permita peticiones desde el cliente
+
+//ARMADO DE RUTAS (endpoints)
 app.get("/health", (req, res) => {
     res.json({ status: "OK" });  //enviar una respuesta en formato json
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT;  //gracias a dotenv, puedo acceder a las variables de entorno de .env
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);   //imprimimos en consola que el servidor se esta ejecutando en el puerto 5000
 });
